faf17b9ed6c0d21d2f39930ed5aab1c39c45a2c1
zach <zaclewis@gmail.com>
2024-11-07 13:23:09 -0600

    ase

---
+++ checkEnv.js
@@ -0,0 +1,8 @@
+const requiredEnvVars = ['TAES_KEY'];
+
+const missingEnvVars = requiredEnvVars.filter((key) => !process.env[key]);
+
+if (missingEnvVars.length > 0) {
+  console.error(`Missing required environment variables: ${missingEnvVars.join(', ')}`);
+  process.exit(1); // Exit with an error code to stop the app
+}
