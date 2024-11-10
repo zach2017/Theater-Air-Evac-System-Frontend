5e09e1110257d0c6390f3c3e67df479c79f2f7cf
zach <zaclewis@gmail.com>
2024-11-10 12:46:45 -0600

    fix layout

--- src/content/ae/PatientListing.js
+++ src/content/ae/PatientListing.js
@@ -7,6 +7,7 @@ import { Box, Grid, IconButton, Tooltip, Typography } from '@mui/material'
 // MUI Icons
 import DeleteIcon from '@mui/icons-material/Delete'
 import PersonAddIcon from '@mui/icons-material/PersonAdd'
+import AddAPhoto from '@mui/icons-material/AddAPhoto'
 
 // custom 
 import useStorage from '../../api/useStorage'
@@ -41,13 +42,29 @@ function PatientListing(props) {
 
     return (
         <>
-            <Grid item xs={12}>
-                <Typography variant="h5">
-                    Patient Listing
-                </Typography>
-            </Grid>
-            {sortedPatients.map((patient, index) => <PatientCard key={index} {...patient} />)}
-            <Grid item xs={2}>
+        <Tooltip
+                title="Add New Patient"
+            >
+                <IconButton
+                    aria-label="Add New Patient"
+                    onClick={() => setOpen(true)}
+                >
+                    <PersonAddIcon fontSize="large" />
+                </IconButton>
+            </Tooltip>
+         <Tooltip
+                    title="Add Patient Image"
+                >
+                    <IconButton
+                        aria-label="Add Patient Image"
+                        onClick={() => setOpen(true)}
+                    >
+                        <AddAPhoto fontSize="large" />
+                    </IconButton>
+                </Tooltip>
+        <Grid item xs={8} />
+               
+                <Box sx={{ flexGrow: 1 }} />
                 <Tooltip
                     title="Delete Patients"
                 >
@@ -58,25 +75,20 @@ function PatientListing(props) {
                         <DeleteIcon fontSize="large" />
                     </IconButton>
                 </Tooltip>
-            </Grid>
-            <Grid item xs={8} />
-            <Grid item xs={2} sx={{ display: "flex" }}>
-                <Box sx={{ flexGrow: 1 }} />
-                <Tooltip
-                    title="Add New Patient"
-                >
-                    <IconButton
-                        aria-label="Add New Patient"
-                        onClick={() => setOpen(true)}
-                    >
-                        <PersonAddIcon fontSize="large" />
-                    </IconButton>
-                </Tooltip>
-            </Grid>
+               
             <NewPatientForm
                 open={open}
                 close={close}
             />
+            <Grid item xs={12}>
+            
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h5">
+                    Patient Listing
+                </Typography>
+            </Grid>
+            {sortedPatients.map((patient, index) => <PatientCard key={index} {...patient} />)}
         </>
     )
 }
